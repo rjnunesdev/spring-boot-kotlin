da8b8fe27eeb421490674101495f273ca01ef07c
Rodrigo Nunes <rjnunes2212@gmail.com>
2023-04-23 13:39:31 -0300

    Base Spring Boot Kotlin

---
+++ spring-boot-kotlin/src/test/kotlin/com/rjnunesdev/vistoriabackend/SpringBootKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rjnunesdev.vistoriabackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
